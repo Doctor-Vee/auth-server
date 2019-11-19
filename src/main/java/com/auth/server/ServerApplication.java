1dd4fa57a55562efd7be79946a1c5720059dc3a7
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-11-19 21:07:55 +0100

    enable authorization server using annotation

--- src/main/java/com/auth/server/ServerApplication.java
+++ src/main/java/com/auth/server/ServerApplication.java
@@ -2,11 +2,14 @@ package com.auth.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class ServerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ServerApplication.class, args);
     }
 
