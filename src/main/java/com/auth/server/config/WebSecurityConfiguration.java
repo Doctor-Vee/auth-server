815e0c6b106402972350886961a79d39d140353b
hayobabson <babalolakehinde.p@gmail.com>
2019-11-19 19:01:38 +0100

    add password encoder type to web security

--- src/main/java/com/auth/server/config/WebSecurityConfiguration.java
+++ src/main/java/com/auth/server/config/WebSecurityConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -20,4 +22,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected AuthenticationManager getAuthenticationManager() throws Exception{
         return super.authenticationManagerBean();
     }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+    
 }
