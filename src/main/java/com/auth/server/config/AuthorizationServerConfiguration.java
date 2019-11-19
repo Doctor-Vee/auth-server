5571ff7e62d14bf99def37a442d662ef38759cc0
hayobabson <babalolakehinde.p@gmail.com>
2019-11-19 19:26:52 +0100

    override spring authorization server interface

--- src/main/java/com/auth/server/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/auth/server/config/AuthorizationServerConfiguration.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
@@ -26,4 +29,21 @@ public class AuthorizationServerConfiguration implements AuthorizationServerConf
         return new JdbcTokenStore(dataSource);
     }
 
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.checkTokenAccess("isAuthenticated()").tokenKeyAccess("permitAll()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer client) throws Exception {
+        client.jdbc(dataSource).passwordEncoder(passwordEncoder);
+
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoint) throws Exception {
+        endpoint.tokenStore(jdbcTokenStore());
+        endpoint.authenticationManager(authenticationManager);
+
+    }
 }
